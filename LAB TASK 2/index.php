e1a0db5941b1c7e4afd1f6e3fc1a267d0bd16a9c
18-38767-3@student.aiub.edu <18-38767-3@student.aiub.edu>
2021-08-02 01:11:12 +0600

    Lab Task AJAX Update

---
+++ LAB TASK 2/index.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<div id="demo">
+<button type="button" onclick="loadDoc()">Click Me</button>
+</div>
+
+<script>
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "myProfile.php");
+  xhttp.send();
+}
+</script>
+
+</body>
+</html>
